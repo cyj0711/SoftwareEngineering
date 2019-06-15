8024958449405fd35efddffe57fc26feed4206e8
kyungsook <33944540+kyungsook@users.noreply.github.com>
2019-06-15 18:57:41 +0900

    Login 화면 구현

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Login POS = new Login();
+		//POS.setVisible(true);
+		
+	}
+
+	
+}
